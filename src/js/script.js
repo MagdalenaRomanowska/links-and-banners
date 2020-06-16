ef11c49701b6c928ff285949665a15fd9625e7e6
Magdalena Romanowska <magdalena.romanowska.poczta@gmail.com>
2020-06-16 19:22:08 +0200

    Dashboard solo project

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+function toggleMenu(visible) {
+  document.querySelector('.container-fluid-left-menu').classList.toggle('d-none', visible);
+}
+  
+document.querySelector('.empty .fa-bars').addEventListener('click', function(e) {
+  e.preventDefault();
+  toggleMenu();
+});
