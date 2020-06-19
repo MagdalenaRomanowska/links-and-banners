2cd4e393f5b1ddaeb38e4e73e576970ece9e8a50
Magdalena Romanowska <magdalena.romanowska.poczta@gmail.com>
2020-06-19 14:06:55 +0200

    Dashboard change, mostly JS

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 function toggleMenu(visible) {
   document.querySelector('.container-fluid-left-menu').classList.toggle('d-none', visible);
+  document.querySelector('.container-fluid-right-menu').classList.toggle('col-lg-12');
 }
   
 document.querySelector('.empty .fa-bars').addEventListener('click', function(e) {
@@ -7,7 +8,29 @@ document.querySelector('.empty .fa-bars').addEventListener('click', function(e) 
   toggleMenu();
 });
 
+function removeMenu() {
+  document.querySelector('.container-fluid-left-menu').classList.add('d-none');
+  document.querySelector('.empty .fa-bars').classList.remove('d-lg-none');
+  document.querySelector('.container-fluid-right-menu').classList.toggle('col-lg-12');
+}
+
 document.querySelector('.left-nav .fa-bars').addEventListener('click', function(e) {
   e.preventDefault();
-  toggleMenu();
+  removeMenu();
 });
+
+function addMenu() {
+  document.querySelector('.container-fluid-left-menu').classList.remove('d-none');
+}
+
+function myFunction(findMediaSize) {
+  if (findMediaSize.matches) { // If media query matches
+    removeMenu();
+  } else {
+    addMenu();
+  }
+}
+
+const findMediaSize = window.matchMedia('(max-width: 700px)');
+myFunction(findMediaSize); 
+findMediaSize.addListener(myFunction); 
